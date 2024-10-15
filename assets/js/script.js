4bcc09b111e42121dd325f6286dc40d185afbded
unknown <mohanad.13.93@gmail.com>
2024-10-15 14:11:16 +0200

    prepare functions in our project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
-console.log("Hello world")
+let runGame=()=>{
+
+}
+
+let checkAnswer=()=>{
+
+}
+
+let calculateCorrectAnswer=()=>{
+
+}
+let incrementScore=()=>{
+
+}
+
+let incrementWrongAnswer=()=>{
+
+}
+
+let displayAdditionQuestion=()=>{
+
+}
+
+let displaySubtractionQuestion=()=>{
+
+}
+
+let displayMultiplyQuestion=()=>{
+
+}
+
+let displayDivisionAnswer=()=>{
+    
+}
