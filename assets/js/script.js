be84d8753e20122ac6d71cede8749c10ee06f62d
unknown <mohanad.13.93@gmail.com>
2024-10-15 14:30:58 +0200

    Add event listener to ContentdomLoaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+
+
+document.addEventListener("DOMContentLoaded",function(){
+
+    let buttons=document.getElementsByTagName("button");
+    for(let button of buttons)
+    {
+
+        button.addEventListener("click",function(){
+            switch(this.getAttribute("data-type").toLowerCase())
+            {
+                case "submission":
+                    alert(`You have clicked submition`);
+                default:
+                    alert(`You have clicked ${this.getAttribute("data-type")} `)
+            }
+        })
+    }
+})
+
 let runGame=()=>{
 
 }
