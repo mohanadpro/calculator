2faed51a40f35c68931387f8f358b3ced85370f9
unknown <mohanad.13.93@gmail.com>
2024-10-15 15:03:34 +0200

    work on math functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,23 +7,50 @@ document.addEventListener("DOMContentLoaded",function(){
     {
 
         button.addEventListener("click",function(){
-            switch(this.getAttribute("data-type").toLowerCase())
+            switch(this.getAttribute("data-type"))
             {
                 case "submission":
                     alert(`You have clicked submition`);
-                default:
-                    alert(`You have clicked ${this.getAttribute("data-type")} `)
+                    break;
+                case "addition":
+                    runGame("addition");
+                    break;
+                case "subtraction":
+                    runGame("subtraction");
+                    break;
+                case "multiply":
+                    runGame("multiply");
+                    break;
+                case "division":
+                    runGame("division");
+                    break;
             }
         })
     }
+    runGame("addition");
 })
 /**
  * This function runs once the DOM is loaded 
  * and after the question is answered
  */
-let runGame=()=>{
+let runGame=(dataType)=>{
     let firstNumber=Math.floor(Math.random()*25)+1;
     let secondNumber=Math.floor(Math.random()*25)+1;
+    switch(dataType)
+    {
+        case "addition":
+            displayAdditionQuestion(firstNumber,secondNumber);
+            break;
+        case "subtraction":
+            displaySubtractionQuestion(firstNumber,secondNumber);
+            break;
+        case "multiply":
+            displayMultiplyQuestion(firstNumber,secondNumber);
+            break;
+        case "division":
+            displayDivisionAnswer(firstNumber,secondNumber);
+            break;
+    }
 }
 
 
@@ -42,18 +69,30 @@ let incrementWrongAnswer=()=>{
 
 }
 
-let displayAdditionQuestion=()=>{
-
+/**
+ * It accepts two parameters
+ * after that it increment them together
+ */
+let displayAdditionQuestion=(firstNumber,secondNumber)=>{
+    document.getElementById("operand1").textContent=firstNumber;
+    document.getElementById("operand2").textContent=secondNumber;
+    document.getElementById("operator").textContent="+";
 }
 
-let displaySubtractionQuestion=()=>{
-
+let displaySubtractionQuestion=(firstNumber,secondNumber)=>{
+    document.getElementById("operand1").textContent=firstNumber;
+    document.getElementById("operand2").textContent=secondNumber;
+    document.getElementById("operator").textContent="-";
 }
 
-let displayMultiplyQuestion=()=>{
-
+let displayMultiplyQuestion=(firstNumber,secondNumber)=>{
+    document.getElementById("operand1").textContent=firstNumber;
+    document.getElementById("operand2").textContent=secondNumber;
+    document.getElementById("operator").textContent="X";
 }
 
-let displayDivisionAnswer=()=>{
-    
+let displayDivisionAnswer=(firstNumber,secondNumber)=>{
+    document.getElementById("operand1").textContent=firstNumber;
+    document.getElementById("operand2").textContent=secondNumber;
+    document.getElementById("operator").textContent="/";
 }
